c4e4c65d13f74e780b8d92181328612bf72a7435
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-05 16:29:57 +0900

    docs : create 6-2.js

---
+++ 6-2-JSON.js
@@ -0,0 +1,7 @@
+/*
+JSON,
+JavaScript Object Notation의 약자로, JS에서 객체를 만들 때 사용하는 표현식을 의미한다.
+이 표현식은 사람도 기계도, 이해하기 쉬우면서 크기도 작게 차지하기에 XML대신 JSON으로 설정을 저장하거나, 데이터를 전송할 때 많이 사용한다.
+
+json 공식 홈페이지 : https://www.json.org/json-ko.html
+*/
