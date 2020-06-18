e18e335bdb97d8ba3e1ccb019dafe4a363963acd
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-18 23:12:59 +0900

    feat : make todo list!

---
+++ JS로 크롬앱 만들기/todo.js
@@ -0,0 +1,37 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+function paintToDo(text) {
+    const li = document.createElement('li');
+    const delBtn = document.createElement('button');
+    delBtn.innerText = '❌';
+    const span = document.createElement('span');
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const Value = toDoInput.value;
+    paintToDo(Value);
+    toDoInput.value = '';
+}
+
+function loadToDos(){
+    const toDos = localStorage.getItem(TODOS_LS);
+    if(toDos !== null){
+
+    }
+}
+
+function init(){
+    loadToDos();
+    toDoForm.addEventListener('submit', handleSubmit)
+}
+
+init();
