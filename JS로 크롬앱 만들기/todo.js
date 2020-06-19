35721625657cf09f4b3622c2e55a183c1f02a41f
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-20 00:43:25 +0900

    feat : make save todo list in localStorage

--- JS로 크롬앱 만들기/todo.js
+++ JS로 크롬앱 만들기/todo.js
@@ -3,18 +3,30 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
-
 const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+
+}
 
 function paintToDo(text) {
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
-    delBtn.innerText = '❌';
     const span = document.createElement('span');
+    const newId = toDos.length + 1;
+    delBtn.innerText = '❌';
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.id = newId;
     toDoList.appendChild(li);
+    toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event){
@@ -25,9 +37,12 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        });
     }
 }
 
